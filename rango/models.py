5e7fc9dfe7d8da2e47fdba7037c76a96429e9a96
“Bayan <3070264a@student.gla.ac.uk>
2025-02-13 13:18:59 +0000

    Added views and likes fields to Category model

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0) #field for tracking views.
+    likes = models.IntegerField(default=0) #field for tracking likes.
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #one to many relationship for each category and pages
+    title = models.CharField(max_length=128)    #title of the page
+    url = models.URLField() #stores webpage url
+    views = models.IntegerField(default=0)  #tracks how many times the page has been viewed
+
+    def __str__(self):
+        return self.title
